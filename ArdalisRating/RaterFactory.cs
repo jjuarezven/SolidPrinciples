0ea3c0a5defd5bc32996c31041a7d55c9fe506f6
Jose Juarez <jose.juarez@realpage.com>
2021-02-02 13:34:54 -0500

    Using reflection for RaterFactory

--- ArdalisRating/RaterFactory.cs
+++ ArdalisRating/RaterFactory.cs
@@ -1,27 +1,37 @@
-﻿namespace ArdalisRating
+﻿using System;
+
+namespace ArdalisRating
 {
     public class RaterFactory
     {
         public Rater Create(Policy policy, RatingEngine engine)
         {
-            switch (policy.Type)
+            try
             {
-                case PolicyType.Auto:
-                    return new AutoPolicyRater(engine, engine.Logger);
-
-                case PolicyType.Flood:
-                    return new FloodPolicyRater(engine, engine.Logger);
-
-                case PolicyType.Land:
-                    return new LandPolicyRater(engine, engine.Logger);
-
-                case PolicyType.Life:
-                    return new LifePolicyRater(engine, engine.Logger);
-
-                default:
-                    // currently this can't be reached 
-                    return new UnknownPolicyRater(engine, engine.Logger);
+                return (Rater)Activator.CreateInstance(Type.GetType($"ArdalisRating.{policy.Type}PolicyRater"), new object[] { engine, engine.Logger });
             }
+            catch
+            {
+                return null;
+            }
+            //switch (policy.Type)
+            //{
+            //    case PolicyType.Auto:
+            //        return new AutoPolicyRater(engine, engine.Logger);
+
+            //    case PolicyType.Flood:
+            //        return new FloodPolicyRater(engine, engine.Logger);
+
+            //    case PolicyType.Land:
+            //        return new LandPolicyRater(engine, engine.Logger);
+
+            //    case PolicyType.Life:
+            //        return new LifePolicyRater(engine, engine.Logger);
+
+            //    default:
+            //        // currently this can't be reached 
+            //        return new UnknownPolicyRater(engine, engine.Logger);
+            //}
         }
     }
 }
